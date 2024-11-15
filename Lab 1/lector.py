ed1194123408f1b981249527275b763cb9804524
danzapata <142749695+danzapata@users.noreply.github.com>
2024-11-14 19:40:08 -0500

    Ejercicio 2 Lab 1

--- Lab 1/lector.py
+++ Lab 1/lector.py
@@ -1,10 +1,12 @@
 # Ejercicio 2 Lab 1
 
 with open("Lab 1/test_pr2.txt", "r") as archivo:
-    # Leemos la lectura y la separamos por los espacios
+    # Leemos la lectura y la separamos 
     lectura = archivo.read()
-    palabras = lectura.split(" ")
 
+    lecc = lectura.replace("\n", " ")
+    palabras = lecc.split(" ")
+    
     # Contamos los En y en
     contador = 0
     for i in palabras:
