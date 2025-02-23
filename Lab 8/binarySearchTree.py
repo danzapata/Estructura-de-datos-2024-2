9e044acab217bedc40647c9422d22f1d0e59b689
danzapata <142749695+danzapata@users.noreply.github.com>
2025-02-22 19:23:27 -0500

    completo árbol binario

--- Lab 8/binarySearchTree.py
+++ Lab 8/binarySearchTree.py
@@ -1,6 +1,7 @@
 # Importaciones
 from binarytree import BinaryTree
 from bstentry import BSTEntry
+from nodoDoble import NodoDoble
 
 # Clase
 class BinarySearchTree(BinaryTree):
@@ -22,4 +23,46 @@ class BinarySearchTree(BinaryTree):
         else:
             return self.searchTree(key, self.right(nodo))
         
-    #
+    # Insertar al árbol 
+    def addEntry(self, nodo, BSTEntry):
+        temp = nodo.getData()
+        nodoNuevo = NodoDoble(BSTEntry)
+        if BSTEntry.getKey()<temp.getKey():
+            if self.hasLeft(nodo):
+                self.addEntry(self.left(nodo), BSTEntry)
+            else:
+                nodo.setPrev(nodoNuevo)
+        else:
+            if self.hasRight(nodo):
+                self.addEntry(self.right(nodo, BSTEntry))
+            else:
+                nodo.setNext(nodoNuevo)
+
+    def insert(self, objeto, key):
+        temp = BSTEntry(objeto, key)
+        if self.isEmpty():
+            self.addRoot(temp)
+        else:
+            self.addEntry(self._root, temp)
+
+    # Eliminar del árbol 
+    def maxNodo(self, nodo):
+        if self.hasRight(nodo):
+            return self.maxNodo(self.right(nodo))
+        else:
+            return nodo
+
+    def predecesor(self, nodo):
+        temp = self.left(nodo)
+        return self.maxNodo(temp)
+
+    def Remove(self, key):
+        v = self.find(key)
+        temp = v.getData()
+        if self.hasLeft(v) and self.hasRight(v):
+            W = self.predecesor(v)
+            v.setData(W.getData())
+            self.remove(W)
+        else:
+            self.remove(v)
+        return temp
